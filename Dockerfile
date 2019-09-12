31d3409717d401c6e404072e82229c28c7ecf5ef
Ric Emery <ric.emery@gmail.com>
2019-09-11 21:43:41 -0700

    Experiment with creating Docker image for scala-test-runner

--- Dockerfile
+++ Dockerfile
@@ -8,14 +8,21 @@ USER root
 RUN apk add --no-cache -X http://dl-cdn.alpinelinux.org/alpine/edge/testing sbt
 
 COPY --chown=gradle:gradle . .
-#RUN sbt assembly
+#RUN sbt -Dsbt.ivy.home=.ivy2 -Dsbt.global.base=.sbt/1.0 -Dsbt.boot.directory=.sbt/boot test
 RUN mkdir -p /opt/scala-test-runner
 RUN mkdir -p /opt/scala-test-runner/lib
 
 #RUN cp /home/gradle/target/scala-2.12/*.jar /opt/scala-test-runner/lib
 COPY bin/run.sh /opt/scala-test-runner/bin/
+#RUN cp -r .ivy2 /opt/scala-test-runner
+#RUN cp -r .sbt /opt/scala-test-runner
+COPY dependencies/ivy2.tar.gz /opt/scala-test-runner
+COPY dependencies/sbt.tar.gz /opt/scala-test-runner
+RUN tar -xvf /opt/scala-test-runner/ivy2.tar.gz -C /opt/scala-test-runner
+RUN tar -xvf /opt/scala-test-runner/sbt.tar.gz -C /opt/scala-test-runner
 
 RUN chown -R gradle:gradle /opt/scala-test-runner
+
 
 WORKDIR /opt/scala-test-runner
 
--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 slug="$1"
 solution_path="$2"
@@ -14,10 +14,20 @@ fi
 echo remove pending annotations
 for f in $solution_path/src/test/scala/*.scala
 do
-  sed  -i '' '/pending/d' $f
+  sed -i '/pending/d' $f
+  sed -i 's/import org.scalatest.{Matchers, FunSuite}/import org.scalatest.{Matchers, FunSuite, CancelAfterFailure}/' $f
+  sed -i 's/FunSuite with Matchers/FunSuite with CancelAfterFailure with Matchers/' $f
 done
 
 echo running tests
 cd $solution_path
-sbt test
 
+
+host=$(hostname)
+echo 127.0.0.1 $host > /etc/hosts
+
+sbt -Dsbt.ivy.home=/opt/scala-test-runner/.ivy2 -Divy.home=/opt/scala-test-runner/.ivy2 -Dsbt.global.base=/opt/scala-test-runner/.sbt/1.0 -Dsbt.boot.directory=/opt/scala-test-runner/.sbt/boot test
+
+# Uncomment below if wanting to grab dependencies for building new release of test runner
+#cp -r /opt/scala-test-runner/.ivy2 .
+#cp -r /opt/scala-test-runner/.sbt .
