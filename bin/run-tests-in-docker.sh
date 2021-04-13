ba18883093e29182bb93987f2448be2ccc47c69c
Erik Schierboom <erik_schierboom@hotmail.com>
2021-04-13 17:09:35 +0200

    Comment code

--- bin/run-tests-in-docker.sh
+++ bin/run-tests-in-docker.sh
@@ -16,9 +16,9 @@
 docker build --rm -t exercism/test-runner .
 
 # Run the Docker image using the settings mimicking the production environment
+# TODO: --read-only
+# TODO: --network none
 docker run \
-    --network none \
-    --read-only \
     --mount type=bind,src="${PWD}/tests",dst=/opt/test-runner/tests \
     --mount type=tmpfs,dst=/tmp \
     --workdir /opt/test-runner \
