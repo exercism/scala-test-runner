ecdba804765920d70b623393c890db95d54f6c31
MeerKatDev <lcampobasso@gmail.com>
2021-04-14 09:29:21 +0200

    dockerfile updated (#7)
    * seed project
    
    * shaped up all the necessary files and code skeleton
    
    * all tests passing
    
    * missing only test_code and output
    
    * fixed tests
    
    * Dockerfile improved
    
    * dockerfile update
    
    * Add dockerignore
    
    * Add .gitattributes
    
    * Allow application to be called with three arguments
    
    * Use test runner source
    
    * Fix spec
    
    * Simplify Dockerfile
    
    * Comment code
    
    * Add expected results files
    
    * Allow running without network
    
    * Remove container after running
    
    * Simplify SBT build
    
    * Add workflow to run tests
    
    * Update README
    
    * Handle build errors
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,12 @@
-FROM gradle:jdk-alpine
+FROM mozilla/sbt:8u232_1.4.5
 
-ARG SBT_VERSION=1.2.8
+WORKDIR /opt/test-runner
 
-USER root
+COPY project/ project/
+COPY src/ src/
+COPY build.sbt build.sbt
+RUN sbt compile
 
-# Install sbt
-RUN apk add --no-cache -X http://dl-cdn.alpinelinux.org/alpine/edge/testing sbt
+COPY . .
 
-COPY --chown=gradle:gradle . .
-#RUN sbt -Dsbt.ivy.home=.ivy2 -Dsbt.global.base=.sbt/1.0 -Dsbt.boot.directory=.sbt/boot test
-RUN mkdir -p /opt/scala-test-runner
-RUN mkdir -p /opt/scala-test-runner/lib
-
-#RUN cp /home/gradle/target/scala-2.12/*.jar /opt/scala-test-runner/lib
-COPY bin/run.sh /opt/scala-test-runner/bin/
-#RUN cp -r .ivy2 /opt/scala-test-runner
-#RUN cp -r .sbt /opt/scala-test-runner
-COPY dependencies/ivy2.tar.gz /opt/scala-test-runner
-COPY dependencies/sbt.tar.gz /opt/scala-test-runner
-RUN tar -xvf /opt/scala-test-runner/ivy2.tar.gz -C /opt/scala-test-runner
-RUN tar -xvf /opt/scala-test-runner/sbt.tar.gz -C /opt/scala-test-runner
-
-RUN chown -R gradle:gradle /opt/scala-test-runner
-
-
-WORKDIR /opt/scala-test-runner
-
-ENTRYPOINT ["/opt/scala-test-runner/bin/run.sh"]
+ENTRYPOINT ["/opt/test-runner/bin/run.sh"]
--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,55 +1,72 @@
-import scala.io.Source
-import org.json.{XML, JSONObject, JSONArray}
-import java.net.URL
+  import scala.io.Source
+  import org.json.{JSONArray, JSONObject, XML}
 
-object Application extends App {
+  import java.io.{File, FileWriter}
 
-  def getTestSuiteObject(filepath: URL): JSONObject = {
-    val bufferedSource = Source.fromURL(filepath)
-    val xml = bufferedSource.mkString
-    bufferedSource.close
-    XML.toJSONObject(xml).getJSONObject("testsuite")
-  }
+  object Application extends App {
+    require(args.length == 3, "Invalid number of arguments. Expected: <build-log-file-path> <test-results-file-path> <results-json-file-path>")
+    val buildLogFilePath = args(0)
+    val testResultsFilePath = args(1)
+    val resultsJsonFilePath = args(2)
 
-  def getTestCasesJSON(filepath: URL): JSONArray = {
-    getTestSuiteObject(filepath).getJSONArray("testcase")
-  }
+    writeResultsJSON(buildLogFilePath, testResultsFilePath, resultsJsonFilePath)
 
-  // log, not xml
-  def findErrorsInLog(logFilePath: URL): String = {
-    val fileSource = Source.fromURL(logFilePath)
-    val rawContent = fileSource.mkString
-    fileSource.close
-    if (rawContent.contains("[error] (Test / compileIncremental) Compilation failed")) rawContent else ""
-  }
+    def writeResultsJSON(buildLogFilePath: String, testResultsFilePath: String, resultsJsonFilePath: String): Unit = {
+      val resultsJsonFile = new File(resultsJsonFilePath)
+      val resultsJsonFileWriter = new FileWriter(resultsJsonFile)
 
-  def toExercismJSON(filepath: URL, logFilePath: URL): JSONObject = {
-    val baseObject = new JSONObject().put("version", 2)
-    val errorMessage = findErrorsInLog(logFilePath)
-    if(!errorMessage.isEmpty) {
-      baseObject
-      .put("status", "error")
-      .put("message", errorMessage)
-    } else {
-      val testSuite = getTestSuiteObject(filepath)
-      val failuresNum = testSuite.getInt("failures")
-      val testCasesArray = testSuite.getJSONArray("testcase")
+      val json = toExercismJSON(buildLogFilePath, testResultsFilePath)
+      json.write(resultsJsonFileWriter)
+      resultsJsonFileWriter.close()
+    }
 
-      val testCases: Array[JSONObject] = (0 until testCasesArray.length).toArray.map(idx => {
-        val o = testCasesArray.getJSONObject(idx)
-        val fail = o.optJSONObject("failure")
-        new JSONObject()
-        .put("name", o.getString("name"))
-        .put("status", if(fail != null) "fail" else "pass" )
-        .put("message", if(fail != null) fail.getString("message") else JSONObject.NULL)
-        .put("output", "TOIMPLEMENT")
-        .put("test_code", "TOIMPLEMENT")
-      })
+    def getTestSuiteObject(testResultsFilePath: String): JSONObject = {
+      val bufferedSource = Source.fromFile(testResultsFilePath)
+      val xml = bufferedSource.mkString
+      bufferedSource.close
+      XML.toJSONObject(xml).getJSONObject("testsuite")
+    }
 
-      baseObject
-      .put("status", if(failuresNum > 0) "fail" else "pass")
-      .put("message", JSONObject.NULL)
-      .put("tests", testCases)
+    def getTestCasesJSON(testResultsFilePath: String): JSONArray = {
+      getTestSuiteObject(testResultsFilePath).getJSONArray("testcase")
+    }
+
+    // log, not xml
+    def findErrorsInLog(buildLogFilePath: String): String = {
+      val fileSource = Source.fromFile(buildLogFilePath)
+      val rawContent = fileSource.mkString
+      fileSource.close
+      if (rawContent.contains("[error] (Compile / compileIncremental) Compilation failed") ||
+          rawContent.contains("[error] (Test / compileIncremental) Compilation failed")) rawContent else ""
+    }
+
+    def toExercismJSON(buildLogFilePath: String, testResultsFilePath: String): JSONObject = {
+      val baseObject = new JSONObject().put("version", 2)
+      val errorMessage = findErrorsInLog(buildLogFilePath)
+      if(!errorMessage.isEmpty) {
+        baseObject
+        .put("status", "error")
+        .put("message", errorMessage)
+      } else {
+        val testSuite = getTestSuiteObject(testResultsFilePath)
+        val failuresNum = testSuite.getInt("failures")
+        val testCasesArray = testSuite.getJSONArray("testcase")
+
+        val testCases: Array[JSONObject] = (0 until testCasesArray.length).toArray.map(idx => {
+          val o = testCasesArray.getJSONObject(idx)
+          val fail = o.optJSONObject("failure")
+          new JSONObject()
+          .put("name", o.getString("name"))
+          .put("status", if(fail != null) "fail" else "pass" )
+          .put("message", if(fail != null) fail.getString("message") else JSONObject.NULL)
+          .put("output", "TOIMPLEMENT")
+          .put("test_code", "TOIMPLEMENT")
+        })
+
+        baseObject
+        .put("status", if(failuresNum > 0) "fail" else "pass")
+        .put("message", JSONObject.NULL)
+        .put("tests", testCases)
+      }
     }
   }
-}
