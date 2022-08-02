3c2e41c20d485aeaa42c8421da2b5edc3a519841
JCranky <paulo.siqueira@gmail.com>
2022-08-02 11:23:23 +0200

    Add support for exercises with Lenses
    This enables compiling exercises and running tests with Monocle Lens.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,12 @@
 import sbt._
 
 object Dependencies {
+  val monocleVersion = "2.0.0"
+
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10"
+
+  lazy val monocle = Seq(
+    "com.github.julien-truffaut" %%  "monocle-core"  % monocleVersion,
+    "com.github.julien-truffaut" %%  "monocle-macro" % monocleVersion
+  )
 }
