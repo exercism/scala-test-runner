9311976198129f3e247e3acaa2f99c4cc0eb527b
Kirill Artamonov <artamonov.kirill@gmail.com>
2023-02-15 21:38:00 +0100

    Add scalacheck dependency
    Required by, e.g., simple-linked-list exercise
    Context: https://forum.exercism.org/t/tests-fail-for-exercise-simple-linked-list/3640/9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,7 @@ object Dependencies {
   val monocleVersion = "2.0.0"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10"
+  lazy val scalaCheck = "org.scalatestplus" %% "scalacheck-1-15" % "3.2.9.0"
 
   lazy val monocle = Seq(
     "com.github.julien-truffaut" %%  "monocle-core"  % monocleVersion,
